deb0a8c7077c7db524c6ac135e78271543730f63
Somibo <emiltmil@gmail.com>
2024-02-08 22:00:22 +0300

    2.1.2.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
+        for (String beanDefinitionName : applicationContext.getBeanDefinitionNames()) { // [ВИДНО НАЗВАНИЕ БИНОВ!]
+            System.out.println(beanDefinitionName);
+        }
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat kot1 = applicationContext.getBean("Cat", Cat.class);
+        Cat kot2 = applicationContext.getBean("Cat", Cat.class);
+
+        System.out.println(bean == bean2);
+        System.out.println(kot1 == kot2);
     }
 }
